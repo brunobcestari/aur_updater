7584c9cfc6f259e93ad94e5106d8434395d18433
brunobcestari <brunobcestari@users.noreply.github.com>
2019-07-07 21:43:42 -0300

    add sed to remove quotes from pkgname

--- aur_updater.sh
+++ aur_updater.sh
@@ -2,23 +2,24 @@
 
 FILE_PATH="$HOME/Aur_Packages"
 
-for files in $(ls $FILE_PATH); do
-	cd $FILE_PATH/$files
+for BASEDIR in $(ls $FILE_PATH); do
+	cd $FILE_PATH/$BASEDIR
 	OUTPUT=$(git pull)
 	if [ "$OUTPUT" == "Already up to date." ]; then
-		echo "$files already up to date."
+		echo "Repository of $BASEDIR already up to date."
 	else
-		echo "$files updated."
+		echo "Repository of $BASEDIR updated."
 	fi
+	PKGNAME=$(cat PKGBUILD | grep "pkgname=" | head -1 | cut -f2 -d"=" | sed "s/'//g")
 	LAST_VER=$(cat PKGBUILD | grep "pkgver=" | head -1 | cut -f2 -d"=")
-	ACTUAL_VER=$(pacman -Qem | grep $files" " | cut -f2 -d" " | cut -f2 -d" " | cut -f1 -d"-")
+	ACTUAL_VER=$(pacman -Qem | grep $PKGNAME" " | cut -f2 -d" " | cut -f2 -d" " | cut -f1 -d"-")
 	echo "Last version: $LAST_VER"
 	echo "Actual version: $ACTUAL_VER"
 
 	if [ "$LAST_VER" != "$ACTUAL_VER" ]; then
 		makepkg -si
 	else
-		echo "$files already up to date"
+		echo "$PKGNAME already up to date"
 	fi
 
 	echo
